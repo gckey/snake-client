e36d5128bdf4d4e3d500f696f9513896a6e88136
Abdurahman <abdorehman@gmail.com>
2023-06-16 17:55:02 -0600

    Destructure 'connect'

--- play.js
+++ play.js
@@ -1,26 +1,6 @@
-const net = require("net");
+/*Destructure 'connect' from the module.exports object of client.js and assigns it to a variable named connect in 'play.js' */
 
-// establish a connection with the game server
-const connect = function() {
-  //create a connection object
-  const conn = net.createConnection({
-    host: 'localhost', // IP address of the server
-    port: 50541 //port # the server is listening on
-  });
+const {connect} = require('./client');
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  //handle connection events
-  conn.on('connect', () => {
-    console.log('Connection established.');
-  });
-
-  conn.on('data', (data) => {
-    console.log('Received data:', data);
-  });
-
-  return conn;
-};
-//call the connect function
-// console.log("Connecting ...");
+console.log("Connecting ...");
 connect();
