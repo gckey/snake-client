bc64d81b45c5f23c08c6227f663db151e06aac46
Abdurahman <abdorehman@gmail.com>
2023-06-17 10:00:40 -0600

    Use setupInput module

--- play.js
+++ play.js
@@ -1,24 +1,7 @@
-const {connect} = require('./client');
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
-
-// setup interface to handle user input from stdin
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-};
-
-//function that will be registered as a callback handler for the data' event on stdin.
-const handleUserInput = function(key) {
-  // \u0003 maps to ctrl+c input
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
 
 setupInput();
