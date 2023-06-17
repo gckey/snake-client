a8589e4ec3e343ad281c79829729f43ca06b8da5
Abdurahman <abdorehman@gmail.com>
2023-06-17 16:59:31 -0600

    Bind movement commands to the WASD keys

--- input.js
+++ input.js
@@ -1,11 +1,13 @@
+let connection; // Stores the active TCP connection object.
 
 // setup interface to handle user input from stdin
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on('data', handleUserInput);
+  stdin.on("data", handleUserInput);
   return stdin;
 };
 
@@ -14,6 +16,19 @@ const handleUserInput = function(key) {
   // \u0003 maps to ctrl+c input
   if (key === '\u0003') {
     process.exit();
+  }
+  // Bind movement commands to the WASD keys
+  if (key === 'w') {
+    connection.write('Move: up');
+  }
+  if (key === 'a') {
+    connection.write('Move: left');
+  }
+  if (key === 's') {
+    connection.write('Move: down');
+  }
+  if (key === 'd') {
+    connection.write('Move: right');
   }
 };
 
