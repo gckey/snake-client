e4d7af243434882dd21bf22eafc985bc98270612
Abdurahman <abdorehman@gmail.com>
2023-06-16 22:38:50 -0600

    Experiment with the move up

--- client.js
+++ client.js
@@ -8,22 +8,26 @@ const connect = function() {
     port: 50541 //port # the server is listening on
   });
 
-  // listen to incomming data
-  conn.on('data', (data) => {
-    console.log('Received data:', data);
-  });
-
-  //handle connection event that print a successful conn msg for the player(s)
-  conn.on('connect', () => {
-    console.log('Successfully connected to game server.');
-  });
-
-  //send(write) data via TCP to server
-  conn.on('connect', () => {
-    conn.write('Name: A1B');
-  });
   // interpret incoming data as text
   conn.setEncoding('utf8');
+
+  //'connect' event listner that print a successful conn msg for the player(s)
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server.');
+    //send(write) data via TCP to server
+    conn.write('Name: ABD');
+    
+    // conn.write('Move: up');
+    // setInterval(() => {
+    //   conn.write('Move: up');
+    // }, 50);
+
+  });
+
+  /* Data event listner, listen to incomming data
+  conn.on('data', (data) => {
+    console.log('Received data:', data);
+  }); */
   
   return conn;
 };
