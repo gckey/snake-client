5f8af88b87f62d7a07bcf1388f131fb4c202833d
Abdurahman <abdorehman@gmail.com>
2023-06-17 19:43:41 -0600

    Modify client.js to require the two constants and the 'connect' func to use the new constants.

--- client.js
+++ client.js
@@ -1,11 +1,12 @@
-
 const net = require('net');
+const { IP, PORT } = require("./constants");
+
 // establish a connection with the game server
 const connect = function() {
   //create a connection object
   const conn = net.createConnection({
-    host: '172.18.215.250', // IP address of the server
-    port: 50541 //port # the server is listening on
+    host: IP, // IP address of the server
+    port: PORT //port # the server is listening on
   });
 
   // interpret incoming data as text
@@ -16,18 +17,7 @@ const connect = function() {
     console.log('Successfully connected to game server.');
     //send(write) data via TCP to server
     conn.write('Name: ABD');
-    
-    // conn.write('Move: up');
-    // setInterval(() => {
-    //   conn.write('Move: up');
-    // }, 50);
-
   });
-
-  /* Data event listner, listen to incomming data
-  conn.on('data', (data) => {
-    console.log('Received data:', data);
-  }); */
   
   return conn;
 };
